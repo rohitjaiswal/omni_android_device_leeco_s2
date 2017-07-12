f5ddf2064188bb6919be20ac4752d50bcab4a06f
Rohit Jaiswal <jaiswal.rohit6@gmail.com>
2017-07-12 23:55:51 +0000

    s2: Build with CLANG
    Signed-off-by: Rohit Jaiswal <jaiswal.rohit6@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,6 +19,9 @@ LOCAL_PATH := device/leeco/s2
 # Headers
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
+# Use CLANG
+USE_CLANG_PLATFORM_BUILD := true
+
 # Platform
 TARGET_BOARD_PLATFORM := msm8952
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno510
